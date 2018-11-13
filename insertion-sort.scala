49d1ba2007794a28dae54ff3a9ca5b67680438fa
Leonid Bobylev <leonbobster@gmail.com>
2018-11-13 12:20:17 +0100

    insertion sort

---
+++ insertion-sort.scala
@@ -0,0 +1,13 @@
+object InsertionSort {
+    def isort(xs: List[Int]): List[Int] = xs match {
+      case List() => List()
+      case y :: ys => insert(y, isort(ys))
+    }
+
+    def insert(x: Int, xs: List[Int]): List[Int] = xs match {
+      case List() => List(x)
+      case y :: ys => if (x <= y) x :: xs else y :: insert(x, ys)
+    }
+
+    println(isort(List(5, 6, 3)))
+}
